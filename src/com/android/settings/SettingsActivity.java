31127a8ee726f20864f34d5ee5c280416b070eb0
longping.zou <longping.zou@ck-telecom.com>
2015-05-27 01:28:38 -0700

    Settings:Not responding In-app search function
        1,Settings ->Buttons ->(Home Button)Long press action ->select In-app search
        2,Long press Home button in Settings
    
    Change-Id: I516db91b8ae8f757a64a8d768cfdd92301af2f12

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -737,6 +737,13 @@ public class SettingsActivity extends SettingsDrawerActivity
         setTitleFromBackStack();
     }
 
+    @Override
+    public boolean onSearchRequested() { // Search key pressed.
+        mSearchMenuItem.expandActionView();
+        switchToSearchResultsFragmentIfNeeded();
+        return true;
+    }
+
     private int setTitleFromBackStack() {
         final int count = getFragmentManager().getBackStackEntryCount();
 
